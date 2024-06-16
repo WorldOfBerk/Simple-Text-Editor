4e4c706b2d36e51cfd7d5ebfc2ace0e4ccf4f212
WorldOfBerk <baris.berk.sengl@hotmail.com>
2024-06-16 22:31:34 +0200

    openfile method implemented

--- notepad/src/file/FileOperationsImpl.java
+++ notepad/src/file/FileOperationsImpl.java
@@ -1,22 +1,36 @@
 package file;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileOperationsImpl implements FileOperations{
 
-    private JFrame jFrame;
+    private JFrame frame;
     private JTextArea textArea;
     private File currentFile;
 
-    public FileOperationsImpl(JFrame jFrame, JTextArea textArea) {
-        this.jFrame = jFrame;
+    public FileOperationsImpl(JFrame frame, JTextArea textArea) {
+        this.frame = frame;
         this.textArea = textArea;
     }
 
 
     @Override
     public void openFile() {
+
+        JFileChooser fileChooser = new JFileChooser();
+        int option = fileChooser.showOpenDialog(frame);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            currentFile = fileChooser.getSelectedFile();
+            try(BufferedReader reader = new BufferedReader(new FileReader(currentFile))){
+                textArea.read(reader, null);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(frame, "File could not be opened!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
 
     }
 
